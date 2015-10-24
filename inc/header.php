cb0aad2a44fd9b8f45fb23d1b67dccb496cf7162
khaledkhalil94 <k.khalil.94@gmail.com>
2015-10-24 07:32:35 +0200

    Added PHP

--- inc/header.php
+++ inc/header.php
@@ -5,8 +5,7 @@
           <div class="quick-links">
             <ul class="links">
               <li>
-                <a href="https://www.facebook.com/%D8%A7%D9%83%D8%A7%D8%AF%D9%8A%D9%85%D9%8A%D8%A9-%D8%A7%D9%84%D8%B4%D8%B1%D9%88%D9%82-223922021273/timeline/?ref=br_tf">
-                  <i class="fa fa-2x fa-facebook-official"></i>
+                <a href="https://www.facebook.com"></i>
                 </a>
               </li>
               <li><a href="#">إتصل بنا</a>

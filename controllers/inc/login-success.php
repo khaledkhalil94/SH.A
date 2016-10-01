95b48d10bb69c6850d2094f8ad84ad57a6282b4c
khaledkhalil94 <k.khalil.94@gmail.com>
2016-10-01 11:15:46 +0200

    Improved the signup/login controller

--- controllers/inc/login-success.php
+++ controllers/inc/login-success.php
@@ -13,6 +13,6 @@ if (empty($_SERVER['HTTP_X_REQUESTED_WITH']) || strtolower($_SERVER['HTTP_X_REQU
 	<i class="check massive green circle icon"></i>
 	<br><br>
 	<div class="header">
-		<h2>Welcome back <?= $_SESSION['username']; ?>!</h2>
+		<h2>Welcome back <?= Session::get('username'); ?>!</h2>
 	</div>
 </div>

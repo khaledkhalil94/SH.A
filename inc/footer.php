be489b92623cc038861914b7f07f44319b448297
khaledkhalil94 <k.khalil.94@gmail.com>
2016-10-07 20:57:24 +0200

    Updated the footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,16 @@
 <div class="footer">
-  <div class="container">
-    <h3>About me:</h3>
-  </div>
+	<div class="container">
+		<div class="ui centered grid">
+		<div class="six wide column">
+			<p>Copyright © <?= date('Y'); ?>, @khaledkhalil94</p>
+		</div>
+		<div class="six wide column">
+			<div class="info">
+				<p><a href="https://github.com/khaledkhalil94/SH.A">About</a></p>
+				<span>-</span>
+				<p><a href="/sha/contact">Contact</a></p>
+			</div>
+		</div>
+	</div>
+</div>
 </div>

f7f94a6742a0d1459d7dc3bd26d96ab2904fd84c
khaledkhalil94 <k.khalil.94@gmail.com>
2016-10-12 18:59:42 +0200

    Added a new method to replace current token with a new one

--- src/Token.php
+++ src/Token.php
@@ -1,21 +1,21 @@
-<?php 
+<?php
 require_once($_SERVER["DOCUMENT_ROOT"] .'/sha/src/init.php');
 
 /**
 * Generates and validates token values to use in forms against CSRF attacks
 */
 class Token {
-	
+
 	/**
 	 * generates a new and unique token
 	 * and returns it after setting it in the session
 	 *
-	 * @return string 
+	 * @return string
 	 */
 	public static function generateToken(){
 
 		if(!Session::get('token')){
-			
+
 			$token = bin2hex(mcrypt_create_iv(32, MCRYPT_DEV_URANDOM));
 
 			Session::set('token', $token);
@@ -25,11 +25,22 @@ class Token {
 		return Session::get('token');
 	}
 
+	/**
+	 * Replace the session token with a new one
+	 */
+	public static function newToken(){
+
+			$token = bin2hex(mcrypt_create_iv(32, MCRYPT_DEV_URANDOM));
+
+			Session::set('token', $token);
+
+	}
+
 
 	/**
 	 * validates a given token with the one stored in the session
 	 *
-	 * @return boolean 
+	 * @return boolean
 	 */
 	public static function validateToken($token){
 

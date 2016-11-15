3c3346e5fc10e5baf0663c8c858629b2f34eac58
khaledkhalil94 <k.khalil.94@gmail.com>
2016-11-15 03:16:56 +0200

    Cleaned the index page a bit

--- index.php
+++ index.php
@@ -1,27 +1,16 @@
 <?php
-require_once ("src/init.php");
+require_once (__DIR__."/src/init.php");
 $pageTitle = "Home Page";
 $sec = "index";
+$pub = (bool) !$session->is_logged_in();
+
 include (ROOT_PATH . "inc/head.php");
-
-$post = new Post();
-
-if($session->is_logged_in()){
-	$feed = $post->get_stream();
-
-	usort($feed, 'date_compare');
-	$feed = array_reverse($feed);
-
-	$feed = array_slice($feed, 0, 30); // limit the feed items to 30
-}
-$pub = $session->is_logged_in() ? true : false;
-$c = !$pub ? 'pub-view' : null;
 ?>
 <body>
-	<div class="ui container section main-page <?= $c ?>">
+	<div class="ui container section main-page <?= $pub ? 'pub-view' : null; ?>">
 		<div class="content front">
 			<?= msgs(); ?>
-			<?php if(!$session->is_logged_in()) { ?>
+			<?php if($pub) { ?>
 			<?php require_once(ROOT_PATH.'inc/main.page.not.logged.php'); ?>
 			<?php } else { ?>
 			<div class="news-feed">
@@ -30,10 +19,7 @@ $c = !$pub ? 'pub-view' : null;
 					</div>
 					<hr class='rev'>
 					<div class="main-feed">
-					<?php
-					if(empty($feed)) echo "<p>There doesn't seem to be anything here, follow some users to see what they are up to!</p>";
-						else require_once(ROOT_PATH.'inc/main.news.feed.php');
-					?>
+					<?php require_once(ROOT_PATH.'inc/main.news.feed.php'); ?>
 					</div>
 			</div>
 			<?php } ?>
